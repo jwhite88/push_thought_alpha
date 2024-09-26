2d6e8a298759b67910b3027097543dc881571ad0
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-09-26 10:50:11 -0700

    cleaned up campaign page. broke bookmark, sharing, and contact form into separate components.

---
+++ components/BookmarkButton.jsx
@@ -0,0 +1,14 @@
+import { FaBookmark } from 'react-icons/fa'
+import React from 'react'
+
+const BookmarkButton = () => {
+  return (
+    <button
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+        >
+        <FaBookmark className="fas mr-2" /> Bookmark Property
+    </button>
+  )
+}
+
+export default BookmarkButton
