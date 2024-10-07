77a655f1ce92c9a12aaad027605fc6f7268bf7a7
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-10-07 14:43:50 -0700

    header color changes.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   }, [])
 
   return (
-    <nav className="bg-blue-700 border-b border-blue-500">
+    <nav className="bg-orange-700 border-b border-orange-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
