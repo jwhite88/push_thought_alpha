591395bb97337d0906b94e997894d1a391b52a12
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-10-15 11:54:25 -0700

    wraped tags on narrow screen or mobile version

--- components/CampaignDetails.jsx
+++ components/CampaignDetails.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
-import { 
-    FaBed,
-    FaBath,
-    FaRulerCombined,
-    FaTimes,
-    FaCheck,
-    FaMapMarker
- } from 'react-icons/fa'
+import {
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaTimes,
+  FaCheck,
+  FaMapMarker
+} from 'react-icons/fa'
 
-const CampaignDetails = ({campaign}) => {
+const CampaignDetails = ({ campaign }) => {
   return (
-        <main>
-            <div
-              className="bg-white p-6 rounded-lg shadow-md text-center md:text-center"
-            >
-              <h1 className="text-3xl font-bold mb-4">{campaign.name}</h1>
-              <p className="text-gray-500 mb-4 text-center">
-                {campaign.description}
-              </p>
-              <div className='campaign-tags mx-auto bg-white'>
-              {campaign.tags.map((item, index) => {
-                return (
-                  <div key={index} className='campaign-tag-items border-black'>
-                    {item}
-                  </div>
-                )
-              } )}
+    <main>
+      <div
+        className="bg-white p-6 rounded-lg shadow-md text-center md:text-center"
+      >
+        <h1 className="text-3xl font-bold mb-4">{campaign.name}</h1>
+        <p className="text-gray-500 mb-4 text-center">
+          {campaign.description}
+        </p>
+        <div className='campaign-tags flex flex-wrap mx-auto bg-white'>
+          {campaign.tags.map((item, index) => {
+            return (
+              <div key={index} className='campaign-tag-items border-black p-1 m-1'>
+                {item}
               </div>
+            )
+          })}
+        </div>
 
-            </div>
-          </main>
+      </div>
+    </main>
   )
 }
 
