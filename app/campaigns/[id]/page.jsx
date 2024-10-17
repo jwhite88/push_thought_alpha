9dea950baa45862db0c5a81a49ce5d17931f2819
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-10-17 14:02:53 -0700

    moved images from across whole screen to just under the description segment of the ui

--- app/campaigns/[id]/page.jsx
+++ app/campaigns/[id]/page.jsx
@@ -67,9 +67,10 @@ const CampaignPage = () => {
         <section className="bg-blue-50">
           <div className="container m-auto py-10 px-6">
             <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-
-              <CampaignDetails campaign={campaign} />
-
+              <div>
+                <CampaignDetails campaign={campaign} />
+                <CampaignImages images={campaign.images} />
+              </div>
               {/* <!-- Sidebar --> */}
               <aside className="space-y-4">
                 {/* <BookmarkButton campaign={campaign}/> */}
@@ -78,11 +79,12 @@ const CampaignPage = () => {
 
 
               </aside>
+
             </div>
           </div>
         </section>
 
-        <CampaignImages images={campaign.images} />
+
 
       </>
       )}
