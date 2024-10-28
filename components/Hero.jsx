64d08f336b58649b2863b2e15c819bb730fdbe1c
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-10-27 19:47:36 -0700

    started search functionality

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,61 +1,27 @@
 import React from 'react'
+import CampaignSearchForm from './CampaignSearchForm'
 
 const Hero = () => {
   return (
     <div>
       {/* // <!-- Hero --> */}
-    <section className="bg-blue-700 py-20 mb-4">
-      <div
-        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
-      >
-        <div className="text-center">
-          <h1
-            className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
-          >
-            Easy political activism
-          </h1>
-          <p className="my-4 text-xl text-white">
-            Contacting congress has never been so quick and simple.
-          </p>
-        </div>
-        <form
-          className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
+      <section className="bg-blue-700 py-20 mb-4">
+        <div
+          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
         >
-          <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
-            <label htmlFor="location" className="sr-only">Location</label>
-            <input
-              type="text"
-              id="location"
-              placeholder="Enter keywords"
-              className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
-            />
-          </div>
-          <div className="w-full md:w-2/5 md:pl-2">
-            <label htmlFor="campaign-type" className="sr-only">Campaign Type</label>
-            <select
-              id="campaign-type"
-              className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+          <div className="text-center">
+            <h1
+              className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
             >
-              <option value="All">All</option>
-              <option value="Apartment">Apartment</option>
-              <option value="Studio">Studio</option>
-              <option value="Condo">Condo</option>
-              <option value="House">House</option>
-              <option value="Cabin Or Cottage">Cabin or Cottage</option>
-              <option value="Loft">Loft</option>
-              <option value="Room">Room</option>
-              <option value="Other">Other</option>
-            </select>
+              Easy political activism
+            </h1>
+            <p className="my-4 text-xl text-white">
+              Contacting congress has never been so quick and simple.
+            </p>
           </div>
-          <button
-            type="submit"
-            className="md:ml-4 mt-4 md:mt-0 w-full md:w-auto px-6 py-3 rounded-lg bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-500"
-          >
-            Search
-          </button>
-        </form>
-      </div>
-    </section>
+          <CampaignSearchForm />
+        </div>
+      </section>
     </div>
   )
 }
--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -12,10 +12,34 @@ import {
 } from 'react-share'
 import { BsTwitterX } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa6";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const ShareButtons = ({ campaign, socialData }) => {
   const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/campaigns/${campaign._id}`
   const [social, setSocial] = useState([])
+
+  const ToastInfo = () => (
+    <div>
+      <h2 style={{ fontSize: '20pt' }} >Did you send your message? Confirm below to be counted in our metrics.</h2>
+      <div>
+        <lable>
+          <span style={{ display: "inline-block", }}>Yes</span> <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "20px", display: "inline-block" }} name="isShared" /> <br />
+          <span style={{ display: "inline-block" }}>No</span>  <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "39px", display: "inline-block" }} name="isShared" />
+        </lable>
+      </div>
+    </div>
+  )
+
+
+  const displayXMsg = () => {
+    toast(<ToastInfo />, {
+      position: "top-center",
+      hideProgressBar: true,
+      autoClose: false
+    })
+
+  }
 
   useEffect(() => {
     console.log("socialData: ", socialData)
@@ -27,10 +51,18 @@ const ShareButtons = ({ campaign, socialData }) => {
   const facebookAddress = campaign?.target_facebook?.address || '';
   const twitterAddress = campaign?.target_x?.address || '';
 
+  const xClosed = () => {
+    console.log("just closed X")
+    displayXMsg()
+  }
+
   return (
     <>
       <h3 className='text-xl font-bold text-center pt-2'>
       </h3>
+      <ToastContainer
+        style={{ marginTop: "300px", fontSize: "52px", width: "600px" }}
+      />
       <div className="flex gap-3 justify-center pb-1 pt-1">
         <FacebookShareButton className='justify-center'
           url={shareUrl}
@@ -46,6 +78,7 @@ const ShareButtons = ({ campaign, socialData }) => {
           // title={social !== null && social.length > 0 && `${social[0]['Twitter']} ${campaign.name}`}
           title={`${twitterAddress} ${campaign.name}`}
           hashtags={[`pushthought`]}
+          onShareWindowClose={xClosed}
         >
           <BsTwitterX size={40} round={true} />
         </TwitterShareButton>
