e2ddf756fb22d7d484aee952d6188c13b3f3fb8f
Matt Acalin <mattacalin@Matthews-MacBook-Pro.local>
2024-10-29 17:26:49 -0700

    added first part of message sent indicators. input icon and created state for hideshow.

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -8,35 +8,67 @@ import {
   TwitterIcon,
   WhatsappIcon,
   EmailIcon,
-
 } from 'react-share'
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+import { ImCheckmark } from "react-icons/im";
 import { BsTwitterX } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa6";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import clsx from 'clsx';
+import "../assets/styles/checkmarks.css"
 
 const ShareButtons = ({ campaign, socialData }) => {
   const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/campaigns/${campaign._id}`
   const [social, setSocial] = useState([])
+  const [isShowingX, setIsShowingX] = useState(true)
+  const [isShowingFB, setIsShowingFB] = useState(false)
+  const [messageSent, setMessageSent] = useState("No")
 
-  const ToastInfo = () => (
-    <div>
-      <h2 style={{ fontSize: '20pt' }} >Did you send your message? Confirm below to be counted in our metrics.</h2>
+  const ToastInfo = ({ messageSent, setMessageSent }) => {
+    const onMessageChange = (event) => {
+      console.log("For sent msg", event.target.value)
+      setMessageSent(event.target.value)
+    }
+
+    return (
       <div>
-        <lable>
-          <span style={{ display: "inline-block", }}>Yes</span> <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "20px", display: "inline-block" }} name="isShared" /> <br />
-          <span style={{ display: "inline-block" }}>No</span>  <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "39px", display: "inline-block" }} name="isShared" />
-        </lable>
+        <h2 style={{ fontSize: '20pt' }} >Did you send your message? Confirm below to be counted in our metrics.</h2>
+        <div>
+          <label>
+            <span style={{ display: "inline-block", }}>Yes</span> <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "20px", display: "inline-block" }} name="isShared" value={"Yes"} onChange={onMessageChange} /> <br />
+          </label>
+          <label>
+            <span style={{ display: "inline-block" }}>No</span>  <input type="radio" style={{ transform: "scale(3.5)", position: "relative", bottom: "10px", left: "39px", display: "inline-block" }} name="isShared" value={"No"} onChange={onMessageChange} />
+          </label>
+          {/* <button onClick={console.log("button firing")}>Submit</button> */}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 
+  const CloseButton = ({ closeToast }) => {
+    return (
+      <i
+        style={{ fontSize: "32px", position: "relative", top: "130px", right: "180px", border: "2px solid black", padding: "10px 8px 20px 8px", width: "auto", height: "70px", fontStyle: "normal", color: "white", backgroundColor: "green", borderRadius: "10px" }}
+        onClick={closeToast}
+      >Submit</i>
+    )
+  }
+
+  const postingMessage = () => {
+    console.log("message value", messageSent)
+  }
 
   const displayXMsg = () => {
-    toast(<ToastInfo />, {
+    toast(<ToastInfo
+      messageSent={messageSent}
+      setMessageSent={setMessageSent}
+    />, {
       position: "top-center",
       hideProgressBar: true,
-      autoClose: false
+      autoClose: false,
+      onClose: postingMessage
     })
 
   }
@@ -62,6 +94,7 @@ const ShareButtons = ({ campaign, socialData }) => {
       </h3>
       <ToastContainer
         style={{ marginTop: "300px", fontSize: "52px", width: "600px" }}
+        closeButton={CloseButton}
       />
       <div className="flex gap-3 justify-center pb-1 pt-1">
         <FacebookShareButton className='justify-center'
@@ -71,6 +104,13 @@ const ShareButtons = ({ campaign, socialData }) => {
         >
           <FaFacebookF size={40} round={true} />
         </FacebookShareButton>
+        <IoCheckmarkCircleSharp
+          style={{ color: "#018749", width: "24px", height: "24px", position: "relative", right: "25px", bottom: "4px" }}
+          className={clsx({
+            'show': isShowingFB,
+            'hide': !isShowingFB
+          })}
+        />
 
         <TwitterShareButton className='justify-center'
           url={shareUrl}
@@ -82,7 +122,13 @@ const ShareButtons = ({ campaign, socialData }) => {
         >
           <BsTwitterX size={40} round={true} />
         </TwitterShareButton>
-
+        <ImCheckmark
+          style={{ color: "#018749", width: "24px", height: "24px", position: "relative", right: "30px", bottom: "4px" }}
+          className={clsx({
+            'show': isShowingX,
+            'hide': !isShowingX
+          })}
+        />
       </div>
     </>
   )
